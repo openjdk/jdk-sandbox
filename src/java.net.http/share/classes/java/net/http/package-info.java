9b814a6cc2bad44478b640a73321b4515c232ca0
Chris Hegarty <chegar@openjdk.org>
2018-02-13 16:22:49 +0000

    Http-client-branch: fix typo in previous push

--- src/java.net.http/share/classes/java/net/http/package-info.java
+++ src/java.net.http/share/classes/java/net/http/package-info.java
@@ -38,7 +38,7 @@
  *
  * <p> The HTTP protocol specific requirements are specified in the
  * <a href="https://tools.ietf.org/html/rfc7540">Hypertext Transfer Protocol
- * Version 2 (HTTP/2)</a>, the <a href=https://tools.ietf.org/html/rfc2616>
+ * Version 2 (HTTP/2)</a>, the <a href="https://tools.ietf.org/html/rfc2616">
  * Hypertext Transfer Protocol (HTTP/1.1)</a>, and
  * <a href="https://tools.ietf.org/html/rfc6455">The WebSocket Protocol</a>.
  *
