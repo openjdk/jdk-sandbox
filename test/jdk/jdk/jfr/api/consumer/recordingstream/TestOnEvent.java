c8cd6ad9bf79b0e29b96b2fbddd247abd3059951
Mikhailo Seledtsov <mseledtsov@openjdk.org>
2019-09-10 09:21:23 -0700

    JEP-349-branch: fixes to copyright headers

--- test/jdk/jdk/jfr/api/consumer/recordingstream/TestOnEvent.java
+++ test/jdk/jdk/jfr/api/consumer/recordingstream/TestOnEvent.java
@@ -22,6 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package jdk.jfr.api.consumer.recordingstream;
 
 import java.util.concurrent.CountDownLatch;
