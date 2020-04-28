5a3d319ed33b11197891852ddfed6acaea233013
Magnus Ihse Bursie <ihse@openjdk.org>
2020-04-28 18:16:47 +0200

    Working with java version

--- make/src/classes/build/tools/pandocfilter/PandocManPageHtmlFilter.java
+++ make/src/classes/build/tools/pandocfilter/PandocManPageHtmlFilter.java
@@ -21,75 +21,96 @@
  * questions.
  */
 
-package build.tools.pandocfilter.json;
+package build.tools.pandocfilter;
+
+import build.tools.pandocfilter.json.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 public class PandocManPageHtmlFilter {
 
-  /**
-   * Traverse a tree of pandoc format objects, calling callback on each
-   * element, and replacing it if callback returns a new object.
-   *
-   * Inspired by the walk method in
-   * https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
-   */
-  private Object traverse(Object obj, Object callback) {
-    if (Array.isArray(obj)) {
-        var processed_array = [];
-        obj.forEach(function(elem) {
-            if (elem === Object(elem) && elem.t) {
-                var replacement = callback(elem.t, elem.c || []);
-                if (!replacement) {
-                    // no replacement object returned, use original
-                    processed_array.push(traverse(elem, callback));
-                } else if (Array.isArray(replacement)) {
-                    // array of objects returned, splice all elements into array
-                    replacement.forEach(function(repl_elem) {
-                        processed_array.push(traverse(repl_elem, callback));
-                    })
-                } else {
-                    // replacement object given, traverse it
-                    processed_array.push(traverse(replacement, callback));
-                }
-            } else {
-                processed_array.push(traverse(elem, callback));
-            }
-        })
-        return processed_array;
-    } else if (obj === Object(obj)) {
-        if (obj.t) {
-            var replacement = callback(obj.t, obj.c || []);
-            if (replacement) {
-                return replacement;
-            }
-        }
-        var processed_obj = {};
-        Object.keys(obj).forEach(function(key) {
-            processed_obj[key] = traverse(obj[key], callback);
-        })
-        return processed_obj;
-    } else {
-        return obj;
+    /**
+     * Traverse a tree of pandoc format objects, calling callback on each
+     * element, and replacing it if callback returns a new object.
+     * <p>
+     * Inspired by the walk method in
+     * https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
+     */
+
+    private static JSONValue callback(String type, JSONValue value) {
+        return null;
     }
-  }
 
-  /*
-   * Helper constructors to create pandoc format objects
-   */
-  private Object Space() {
-    return { 't': 'Space' };
-  }
+    private static JSONValue traverse(JSONValue obj, Object callback2) {
+        if (obj instanceof JSONArray) {
+            JSONArray array = (JSONArray) obj;
 
-  private Object Str(value) {
-    return { 't': 'Str', 'c': value };
-  }
+            JSONArray processed_array = new JSONArray();
+            for (JSONValue elem : array) {
+                if (elem instanceof JSONObject && elem.contains("t")) {
+                    JSONValue replacement = callback(elem.get("t").asString(), elem.contains("c") ? elem.get("c") : new JSONArray());
+                    if (replacement == null) {
+                        // no replacement object returned, use original
+                        processed_array.add(traverse(elem, callback2));
+                    } else if (replacement instanceof JSONArray) {
+                        // array of objects returned, splice all elements into array
+                        JSONArray replacement_array = (JSONArray) replacement;
+                        for (JSONValue repl_elem : replacement_array) {
+                            processed_array.add(traverse(repl_elem, callback2));
+                        }
+                    } else {
+                        // replacement object given, traverse it
+                        processed_array.add(traverse(replacement, callback2));
+                    }
+                } else {
+                    processed_array.add(traverse(elem, callback2));
+                }
+            }
+            return processed_array;
+        } else if (obj instanceof JSONObject) {
+            if (obj.contains("t")) {
+                JSONValue replacement = callback(obj.get("t").asString(), obj.contains("c") ? obj.get("c") : new JSONArray());
+                if (replacement != null) {
+                    return replacement;
+                }
+            }
+            JSONObject obj_obj = (JSONObject) obj;
+            var processed_obj = new JSONObject();
+            for (String key : obj_obj.keys()) {
+                processed_obj.put(key, traverse(obj_obj.get(key), callback2));
+            }
+            return processed_obj;
+        } else {
+            return obj;
+        }
+    }
 
-  private Object MetaInlines(value) {
-    return { 't': 'MetaInlines', 'c': value };
-  }
+    /*
+     * Helper constructors to create pandoc format objects
+     */
+    private Object Space() {
+        return new JSONObject(Map.of(
+                "t", new JSONString("Space")));
+    }
 
-  private Object change_title(type, value) {
-    if (type === 'MetaInlines') {
-        if (value[0].t === 'Str') {
+    private Object Str(JSONValue value) {
+        return new JSONObject(Map.of(
+                "t", new JSONString("Str"),
+                "c", value));
+    }
+
+    private Object MetaInlines(JSONValue value) {
+        return new JSONObject(Map.of(
+                "t", new JSONString("'MetaInlines'"),
+                "c", value));
+    }
+
+    private JSONValue change_title(String type, JSONValue value) {
+        if (type.equals("MetaInlines")) {
+            if (value.get(0).get("t").asString().equals("Str")) {
+            /*
             var match = value[0].c.match(/^([A-Z0-9]+)\([0-9]+\)$/);
             if (match) {
                 return MetaInlines([
@@ -98,30 +119,33 @@ public class PandocManPageHtmlFilter {
 			Space(), Str("Command")
 		    ]);
             }
+            */
+            }
         }
-    }
-  }
-
-  /**
-   * Main function
-   */
-  public static int main(String[] args) {
-    var input = "";
-    while (line = readLine()) {
-        input = input.concat(line);
+        return null;
     }
 
-    var json = JSON.parse(input);
+    /**
+     * Main function
+     */
+    public static void main(String[] args) {
+        StringBuffer input = new StringBuffer();
+        new BufferedReader(new InputStreamReader(System.in)).lines().forEach(line -> input.append(line));
 
-    var meta = json.meta;
-    if (meta) {
-        meta.date = undefined;
-        var title = meta.title;
-        if (meta.title) {
-            meta.title = traverse(meta.title, change_title);
+        JSONValue json = JSON.parse(input.toString());
+
+        JSONValue meta = json.get("meta");
+        if (meta != null && meta instanceof JSONObject) {
+            JSONObject metaobj = (JSONObject) meta;
+            metaobj.remove("date");
+            JSONValue title = meta.get("title");
+            if (title != null) {
+//            metaobj.put(title, traverse(meta.title, change_title));
+                metaobj.put("title", traverse(title, null));
+            }
         }
-    }
 
-    print(JSON.stringify(json));
-  }
+        System.out.println(json);
+
+    }
 }
--- make/src/classes/build/tools/pandocfilter/PandocManPageTroffFilter.java
+++ make/src/classes/build/tools/pandocfilter/PandocManPageTroffFilter.java
@@ -21,125 +21,125 @@
  * questions.
  */
 
-package build.tools.pandocfilter.json;
+package build.tools.pandocfilter;
 
 public class PandocManPageTroffFilter {
-
-  /**
-   * Traverse a tree of pandoc format objects, calling callback on each
-   * element, and replacing it if callback returns a new object.
-   *
-   * Inspired by the walk method in
-   * https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
-   */
-  private Object traverse(Object obj, Object callback) {
-    if (Array.isArray(obj)) {
-        var processed_array = [];
-        obj.forEach(function(elem) {
-            if (elem === Object(elem) && elem.t) {
-                var replacement = callback(elem.t, elem.c || []);
-                if (!replacement) {
-                    // no replacement object returned, use original
-                    processed_array.push(traverse(elem, callback));
-                } else if (Array.isArray(replacement)) {
-                    // array of objects returned, splice all elements into array
-                    replacement.forEach(function(repl_elem) {
-                        processed_array.push(traverse(repl_elem, callback));
-                    })
-                } else {
-                    // replacement object given, traverse it
-                    processed_array.push(traverse(replacement, callback));
-                }
-            } else {
-                processed_array.push(traverse(elem, callback));
-            }
-        })
-        return processed_array;
-    } else if (obj === Object(obj)) {
-        var processed_obj = {};
-        Object.keys(obj).forEach(function(key) {
-            processed_obj[key] = traverse(obj[key], callback);
-        })
-        return processed_obj;
-    } else {
-        return obj;
-    }
-  }
-
-  /*
-   * Helper constructors to create pandoc format objects
-   */
-  private Object Space() {
-    return { 't': 'Space', 'c': [] };
-  }
-
-  private Object Str(value) {
-    return { 't': 'Str', 'c': value };
-  }
-
-  private Object Strong(value) {
-    return { 't': 'Strong', 'c': value };
-  }
-
-  private Object Header(value) {
-    return { 't': 'Header', 'c': value };
-  }
-
-  /**
-   * Callback to change all Str texts to upper case
-   */
-  private Object uppercase(type, value) {
-    if (type === 'Str') {
-        return Str(value.toUpperCase());
-    }
-  }
-
-  /**
-   * Main callback function that performs our man page AST rewrites
-   */
-  private Object manpage_filter(type, value) {
-    // If it is a header, decrease the heading level by one, and
-    // if it is a level 1 header, convert it to upper case.
-    if (type === 'Header') {
-        value[0] = Math.max(1, value[0] - 1);
-        if (value[0] == 1) {
-            return Header(traverse(value, uppercase));
-        }
-    }
-
-    // Man pages does not have superscript. We use it for footnotes, so
-    // enclose in [...] for best representation.
-    if (type === 'Superscript') {
-        return [ Str('['), value[0], Str(']') ];
-    }
-
-    // If it is a link, put the link name in bold. If it is an external
-    // link, put it in brackets. Otherwise, it is either an internal link
-    // (like "#next-heading"), or a relative link to another man page
-    // (like "java.html"), so remove it for man pages.
-    if (type === 'Link') {
-        var target = value[2][0];
-        if (target.match(/^http[s]?:/)) {
-            return [ Strong(value[1]), Space(), Str('[' + target + ']') ];
-        } else {
-            return Strong(value[1]);
-        }
-    }
-  }
-
-  /**
-   * Main function
-   */
-  public static int main(String[] args) {
-    var input = "";
-    while (line = readLine()) {
-        input = input.concat(line);
-    }
-    var json = JSON.parse(input);
-
-    var transformed_json = traverse(json, manpage_filter);
-
-    print(JSON.stringify(transformed_json));
-  }
+//
+//  /**
+//   * Traverse a tree of pandoc format objects, calling callback on each
+//   * element, and replacing it if callback returns a new object.
+//   *
+//   * Inspired by the walk method in
+//   * https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
+//   */
+//  private Object traverse(Object obj, Object callback) {
+//    if (Array.isArray(obj)) {
+//        var processed_array = [];
+//        obj.forEach(function(elem) {
+//            if (elem === Object(elem) && elem.t) {
+//                var replacement = callback(elem.t, elem.c || []);
+//                if (!replacement) {
+//                    // no replacement object returned, use original
+//                    processed_array.push(traverse(elem, callback));
+//                } else if (Array.isArray(replacement)) {
+//                    // array of objects returned, splice all elements into array
+//                    replacement.forEach(function(repl_elem) {
+//                        processed_array.push(traverse(repl_elem, callback));
+//                    })
+//                } else {
+//                    // replacement object given, traverse it
+//                    processed_array.push(traverse(replacement, callback));
+//                }
+//            } else {
+//                processed_array.push(traverse(elem, callback));
+//            }
+//        })
+//        return processed_array;
+//    } else if (obj === Object(obj)) {
+//        var processed_obj = {};
+//        Object.keys(obj).forEach(function(key) {
+//            processed_obj[key] = traverse(obj[key], callback);
+//        })
+//        return processed_obj;
+//    } else {
+//        return obj;
+//    }
+//  }
+//
+//  /*
+//   * Helper constructors to create pandoc format objects
+//   */
+//  private Object Space() {
+//    return { 't': 'Space', 'c': [] };
+//  }
+//
+//  private Object Str(value) {
+//    return { 't': 'Str', 'c': value };
+//  }
+//
+//  private Object Strong(value) {
+//    return { 't': 'Strong', 'c': value };
+//  }
+//
+//  private Object Header(value) {
+//    return { 't': 'Header', 'c': value };
+//  }
+//
+//  /**
+//   * Callback to change all Str texts to upper case
+//   */
+//  private Object uppercase(type, value) {
+//    if (type === 'Str') {
+//        return Str(value.toUpperCase());
+//    }
+//  }
+//
+//  /**
+//   * Main callback function that performs our man page AST rewrites
+//   */
+//  private Object manpage_filter(type, value) {
+//    // If it is a header, decrease the heading level by one, and
+//    // if it is a level 1 header, convert it to upper case.
+//    if (type === 'Header') {
+//        value[0] = Math.max(1, value[0] - 1);
+//        if (value[0] == 1) {
+//            return Header(traverse(value, uppercase));
+//        }
+//    }
+//
+//    // Man pages does not have superscript. We use it for footnotes, so
+//    // enclose in [...] for best representation.
+//    if (type === 'Superscript') {
+//        return [ Str('['), value[0], Str(']') ];
+//    }
+//
+//    // If it is a link, put the link name in bold. If it is an external
+//    // link, put it in brackets. Otherwise, it is either an internal link
+//    // (like "#next-heading"), or a relative link to another man page
+//    // (like "java.html"), so remove it for man pages.
+//    if (type === 'Link') {
+//        var target = value[2][0];
+//        if (target.match(/^http[s]?:/)) {
+//            return [ Strong(value[1]), Space(), Str('[' + target + ']') ];
+//        } else {
+//            return Strong(value[1]);
+//        }
+//    }
+//  }
+//
+//  /**
+//   * Main function
+//   */
+//  public static int main(String[] args) {
+//    var input = "";
+//    while (line = readLine()) {
+//        input = input.concat(line);
+//    }
+//    var json = JSON.parse(input);
+//
+//    var transformed_json = traverse(json, manpage_filter);
+//
+//    print(JSON.stringify(transformed_json));
+//  }
 
 }
