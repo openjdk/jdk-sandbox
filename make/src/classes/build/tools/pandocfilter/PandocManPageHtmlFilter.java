e6b7a68c4f4be49edf7cf3a1a185606fe945eb2f
Magnus Ihse Bursie <ihse@openjdk.org>
2020-06-02 13:23:55 +0200

    Extract common code in a superclass

--- make/src/classes/build/tools/pandocfilter/PandocManPageHtmlFilter.java
+++ make/src/classes/build/tools/pandocfilter/PandocManPageHtmlFilter.java
@@ -23,98 +23,15 @@
 
 package build.tools.pandocfilter;
 
-import build.tools.pandocfilter.json.*;
+import build.tools.pandocfilter.json.JSONArray;
+import build.tools.pandocfilter.json.JSONObject;
+import build.tools.pandocfilter.json.JSONValue;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PandocManPageHtmlFilter {
-    public interface Callback {
-        JSONValue invoke(String type, JSONValue value);
-    }
-
-    /**
-     * Traverse a tree of pandoc format objects, calling callback on each
-     * element, and replacing it if callback returns a new object.
-     * <p>
-     * Inspired by the walk method in
-     * https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
-     */
-
-    private JSONValue traverse(JSONValue obj, Callback callback) {
-        if (obj instanceof JSONArray) {
-            JSONArray array = (JSONArray) obj;
-
-            JSONArray processed_array = new JSONArray();
-            for (JSONValue elem : array) {
-                if (elem instanceof JSONObject && elem.contains("t")) {
-                    JSONValue replacement = callback.invoke(elem.get("t").asString(), elem.contains("c") ? elem.get("c") : new JSONArray());
-                    if (replacement == null) {
-                        // no replacement object returned, use original
-                        processed_array.add(traverse(elem, callback));
-                    } else if (replacement instanceof JSONArray) {
-                        // array of objects returned, splice all elements into array
-                        JSONArray replacement_array = (JSONArray) replacement;
-                        for (JSONValue repl_elem : replacement_array) {
-                            processed_array.add(traverse(repl_elem, callback));
-                        }
-                    } else {
-                        // replacement object given, traverse it
-                        processed_array.add(traverse(replacement, callback));
-                    }
-                } else {
-                    processed_array.add(traverse(elem, callback));
-                }
-            }
-            return processed_array;
-        } else if (obj instanceof JSONObject) {
-            if (obj.contains("t")) {
-                JSONValue replacement = callback.invoke(obj.get("t").asString(), obj.contains("c") ? obj.get("c") : new JSONArray());
-                if (replacement != null) {
-                    return replacement;
-                }
-            }
-            JSONObject obj_obj = (JSONObject) obj;
-            var processed_obj = new JSONObject();
-            for (String key : obj_obj.keys()) {
-                processed_obj.put(key, traverse(obj_obj.get(key), callback));
-            }
-            return processed_obj;
-        } else {
-            return obj;
-        }
-    }
-
-    private JSONValue PandocAtom(String type, JSONValue content) {
-        if (content == null) {
-            return new JSONObject(Map.of(
-                    "t", new JSONString(type)));
-        } else {
-            return new JSONObject(Map.of(
-                    "t", new JSONString(type),
-                    "c", content));
-        }
-    }
-
-    private JSONValue PandocAtom(String type) {
-        return PandocAtom(type, null);
-    }
-
-    /*
-     * Helper constructors to create pandoc format objects
-     */
-    private JSONValue Space() {
-        return PandocAtom("Space");
-    }
-
-    private JSONValue Str(String string) {
-        return PandocAtom("Str", new JSONString(string));
-    }
+public class PandocManPageHtmlFilter extends PandocFilter {
 
     private JSONValue MetaInlines(JSONValue value) {
         return PandocAtom("MetaInlines", value);
@@ -143,16 +60,8 @@ public class PandocManPageHtmlFilter {
      * Main function
      */
     public static void main(String[] args) throws FileNotFoundException {
-        StringBuffer input = new StringBuffer();
-        InputStreamReader reader;
-        if (args.length > 0)
-            reader = new FileReader(args[0]);
-        else {
-            reader = new InputStreamReader(System.in);
-        }
-        new BufferedReader(reader).lines().forEach(line -> input.append(line));
+        JSONValue json = loadJson(args);
 
-        JSONValue json = JSON.parse(input.toString());
         PandocManPageHtmlFilter filter = new PandocManPageHtmlFilter();
 
         JSONValue meta = json.get("meta");
