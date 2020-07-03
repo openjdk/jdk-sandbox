177fe4a0c025d64fedfff99bedf5ceda1ca68c3b
Chris Hegarty <chegar@openjdk.org>
2020-07-03 13:56:46 +0100

    Unixdomainchannels: Improve serializability of UnixDomainSocketAddress

--- test/jdk/java/nio/channels/unixdomain/UnixDomainSocketAddressSerializationTest.java
+++ test/jdk/java/nio/channels/unixdomain/UnixDomainSocketAddressSerializationTest.java
@@ -25,15 +25,19 @@ import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.io.Serializable;
 import java.nio.channels.UnixDomainSocketAddress;
 import java.nio.file.Path;
-
+import static java.io.ObjectStreamConstants.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.expectThrows;
 
 /*
  * @test
@@ -60,6 +64,24 @@ public class UnixDomainSocketAddressSerializationTest {
         assertEquals(deserialized, addr);
     }
 
+    static final Class<InvalidObjectException> IOE = InvalidObjectException.class;
+    static final Class<NullPointerException> NPE = NullPointerException.class;
+
+    /** Tests that UnixDomainSocketAddress in the byte-stream is disallowed. */
+    public static void testUnixDomainSocketAddressInStream() throws Exception {
+        long suid = ObjectStreamClass.lookup(UnixDomainSocketAddress.class).getSerialVersionUID();
+        byte[] bytes = byteStreamFor(UnixDomainSocketAddress.class.getName(), suid);
+        expectThrows(IOE, () -> deserialize(bytes, UnixDomainSocketAddress.class));
+    }
+
+    /** Tests that SerialProxy with a null/absent path value in the byte-stream is disallowed. */
+    public static void testSerialProxyNoStreamValues() throws Exception {
+        Class<?> c = Class.forName("java.nio.channels.UnixDomainSocketAddress$SerialProxy");
+        long suid = ObjectStreamClass.lookup(c).getSerialVersionUID();
+        byte[] bytes = byteStreamFor(c.getName(), suid);
+        expectThrows(NPE, () -> deserialize(bytes, UnixDomainSocketAddress.class));
+    }
+
     private static <T extends Serializable> byte[] serialize(T t)
             throws IOException {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -77,5 +99,27 @@ public class UnixDomainSocketAddressSerializationTest {
             Object o = ois.readObject();
             return cl.cast(o);
         }
+    }
+
+    /**
+     * Returns a stream with the given classname and suid. The stream will have
+     * no stream field values.
+     */
+    static byte[] byteStreamFor(String classname, long suid) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        dos.writeShort(STREAM_MAGIC);
+        dos.writeShort(STREAM_VERSION);
+        dos.writeByte(TC_OBJECT);
+        dos.writeByte(TC_CLASSDESC);
+        dos.writeUTF(classname);
+        dos.writeLong(suid);
+        dos.writeByte(SC_SERIALIZABLE);
+        dos.writeShort(0);                // number of stream fields
+        dos.writeByte(TC_ENDBLOCKDATA);   // no annotations
+        dos.writeByte(TC_NULL);           // no superclasses
+        dos.write(TC_ENDBLOCKDATA);       // end block - for SC_WRITE_METHOD
+        dos.close();
+        return baos.toByteArray();
     }
 }
