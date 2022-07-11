364a0e643d53c19be852f040cd55011363ce18d2
Rafael Winterhalter <rafael.wth@gmail.com>
2022-07-11 23:54:03 +0200

    Adjust javap implementation to be identical to its ASM-based implementation.
    1. Never print labels but byte code offsets.
    2. Do not print the (implicit) abstract flag for inner interfaces or annotations.
    3. Do not print the count for static invocations on interfaces.
    4. Print the JVMS "REF" value for method handle entries instead of the enum name. (Arguably, the enum name contains more information if a target is an interface, but those constants are not mentioned in the specification, this is why I found them confusing when using javap.)
    5. Use the "bsmIndex" for dynamic constant pool entries.
    6. Do not print interpreted data when displaying frames. (Even in previous 'javap' versions, the *chop* frame display did not mention the number of reduced slots, but a class file does not contain the types of frames that are chopped, only the amount. Therefore, I think it is more correct to display this amount rather than the interpreted data.)

---
+++ test/langtools/tools/javap/InnerClass.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8032869
+ * @summary javap should render inner classes in a friendly way
+ * @modules jdk.jdeps/com.sun.tools.javap
+ */
+
+import java.io.*;
+import java.util.function.Function;
+import javax.lang.model.element.ElementKind;
+
+public class InnerClass {
+    public static void main(String... args) throws Exception {
+        new InnerClass().run();
+    }
+
+    void run() throws Exception {
+        String testClasses = System.getProperty("test.classes");
+        String out = javap("-v", "-classpath", testClasses, A.class.getName());
+
+        String nl = System.getProperty("line.separator");
+        out = out.replaceAll(nl, "\n");
+
+        if (out.contains("\n\n\n"))
+            error("double blank line found");
+
+        expect(out,
+                "InnerClasses:\n" +
+                        "  public static #17= #7 of #14;           // A=class InnerClass$A of class InnerClass\n" +
+                        "  public #20= #18 of #7;                  // L=class InnerClass$A$L of class InnerClass$A\n" +
+                        "  protected #23= #21 of #7;               // K=class InnerClass$A$K of class InnerClass$A\n" +
+                        "  static #29= #27 of #7;                  // I=class InnerClass$A$I of class InnerClass$A\n" +
+                        "  static #32= #30 of #7;                  // H=class InnerClass$A$H of class InnerClass$A\n" +
+                        "  static final #35= #33 of #7;            // G=class InnerClass$A$G of class InnerClass$A\n" +
+                        "  static #38= #36 of #7;                  // F=class InnerClass$A$F of class InnerClass$A\n" +
+                        "  static final #41= #39 of #7;            // E=class InnerClass$A$E of class InnerClass$A\n" +
+                        "  final #44= #42 of #7;                   // D=class InnerClass$A$D of class InnerClass$A\n" +
+                        "  abstract #47= #45 of #7;                // C=class InnerClass$A$C of class InnerClass$A\n" +
+                        "  #50= #48 of #7;                         // B=class InnerClass$A$B of class InnerClass$A");
+
+        if (errors > 0)
+            throw new Exception(errors + " errors found");
+    }
+
+    String javap(String... args) throws Exception {
+        StringWriter sw = new StringWriter();
+        int rc;
+        try (PrintWriter out = new PrintWriter(sw)) {
+            rc = com.sun.tools.javap.Main.run(args, out);
+        }
+        System.out.println(sw.toString());
+        if (rc < 0)
+            throw new Exception("javap exited, rc=" + rc);
+        return sw.toString();
+    }
+
+    void expect(String text, String expect) {
+        if (!text.contains(expect))
+            error("expected text not found");
+    }
+
+    void error(String msg) {
+        System.out.println("Error: " + msg);
+        errors++;
+    }
+
+    int errors;
+
+    /* Simple test classes to run through javap. */
+    public static class A {
+
+        class B { }
+        abstract class C { }
+        final class D { }
+        static final class E { }
+        static class F { }
+        static final class G { }
+        interface H { }
+        @interface I { }
+        private class J { }
+        protected class K { }
+        public class L { }
+    }
+}
