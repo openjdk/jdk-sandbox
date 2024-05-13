bc13142918fa6b24519d094c5cb9417020e53f0c
Johannes Bechberger <johannes.bechberger@sap.com>
2024-05-13 10:40:48 +0200

    Add missing files

---
+++ src/hotspot/share/jfr/recorder/stacktrace/jfrAsyncStackTrace.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2011, 2024, SAP SE, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_JFR_RECORDER_STACKTRACE_JFRASYNCSTACKTRACE_HPP
+#define SHARE_JFR_RECORDER_STACKTRACE_JFRASYNCSTACKTRACE_HPP
+#include "jfr/recorder/stacktrace/jfrStackTrace.hpp"
+#include "jfr/utilities/jfrAllocation.hpp"
+#include "jfr/utilities/jfrTypes.hpp"
+#include "oops/method.hpp"
+
+class frame;
+class InstanceKlass;
+class JavaThread;
+class JfrStackTrace;
+
+class JfrAsyncStackFrame {
+  friend class JfrAsyncStackTrace;
+ private:
+  const InstanceKlass* _klass;
+  const Method* _method;
+  int _line;
+  int _bci;
+  u1 _type;
+
+ public:
+  JfrAsyncStackFrame(const Method* _method, int bci, u1 type, int lineno, const InstanceKlass* klass);
+
+  enum : u1 {
+    FRAME_INTERPRETER = 0,
+    FRAME_JIT,
+    FRAME_INLINE,
+    FRAME_NATIVE,
+    NUM_FRAME_TYPES
+  };
+};
+
+class JfrAsyncStackTrace : public JfrCHeapObj {
+  friend class JfrCPUTimeTrace;
+ private:
+  JfrAsyncStackFrame* _frames;
+  u4 _nr_of_frames;
+  u4 _max_frames;
+  bool _reached_root;
+
+  void set_nr_of_frames(u4 nr_of_frames) { _nr_of_frames = nr_of_frames; }
+  void set_reached_root(bool reached_root) { _reached_root = reached_root; }
+  void resolve_linenos() const;
+
+  bool record_async(JavaThread* other_thread, const frame& frame);
+
+  bool full_stacktrace() const { return _reached_root; }
+
+  JfrAsyncStackTrace(JfrAsyncStackFrame* frames, u4 max_frames);
+
+ public:
+
+  // store the trace in a JfrStackTrace object, resolving methods and line numbers
+  bool store(JfrStackTrace* trace, const JfrBuffer* const enqueue_buffer) const;
+
+  u4 nr_of_frames() const { return _nr_of_frames; }
+};
+
+#endif // SHARE_JFR_RECORDER_STACKTRACE_JFRASYNCSTACKTRACE_HPP
