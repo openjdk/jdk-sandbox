c2404d10fc1e6021d5d548f709bdee18c031600e
Justin Lu <justin.lu@oracle.com>
2025-01-06 09:38:40 -0800

    simplify JsonBooleanImpl.toString()

--- src/java.base/share/classes/java/util/json/JsonBooleanImpl.java
+++ src/java.base/share/classes/java/util/json/JsonBooleanImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -84,7 +84,7 @@ final class JsonBooleanImpl implements JsonBoolean, JsonValueImpl {
 
     @Override
     public String toString() {
-        return Boolean.valueOf(value()).toString();
+        return String.valueOf(value());
     }
 
     @Override
