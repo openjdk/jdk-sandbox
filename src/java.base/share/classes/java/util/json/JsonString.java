fc42de114888c81fc270e51824211dc68b9c5fdb
Justin Lu <justin.lu@oracle.com>
2025-01-06 10:17:48 -0800

    'object' is redundant in JsonString::of

--- src/java.base/share/classes/java/util/json/JsonString.java
+++ src/java.base/share/classes/java/util/json/JsonString.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -49,7 +49,7 @@ public sealed interface JsonString extends JsonValue permits JsonStringImpl {
 
     /**
      * {@return the {@code JsonString} created from the given
-     * {@code String} object}
+     * {@code String}}
      *
      * @param src the given {@code String}. Non-null.
      */
