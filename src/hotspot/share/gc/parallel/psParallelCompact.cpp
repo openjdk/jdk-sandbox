dcd457f8a50c45ebfe9a2f2bf8a80ca07bca451b
Roman Kennke <rkennke@openjdk.org>
2025-04-10 11:44:19 +0000

    8347711: [Lilliput] Parallel GC support for compact identity hashcode
    Reviewed-by: stuefe

--- src/hotspot/share/gc/parallel/psParallelCompact.cpp
+++ src/hotspot/share/gc/parallel/psParallelCompact.cpp
@@ -515,7 +515,7 @@ ParallelCompactData PSParallelCompact::_summary_data;
 
 PSParallelCompact::IsAliveClosure PSParallelCompact::_is_alive_closure;
 
-class PCAdjustPointerClosure: public BasicOopIterateClosure {
+class PCAdjustPointerClosureNew: public BasicOopIterateClosure {
   template <typename T>
   void do_oop_work(T* p) { PSParallelCompact::adjust_pointer(p); }
 
@@ -526,7 +526,7 @@ public:
   virtual ReferenceIterationMode reference_iteration_mode() { return DO_FIELDS; }
 };
 
-static PCAdjustPointerClosure pc_adjust_pointer_closure;
+static PCAdjustPointerClosureNew pc_adjust_pointer_closure;
 
 bool PSParallelCompact::IsAliveClosure::do_object_b(oop p) { return mark_bitmap()->is_marked(p); }
 
