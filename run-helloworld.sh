2ba1162a8620eb55ab8040780848e365c1098ea6
Aleksey Shipilev <aleksey@shipilev.net>
2026-03-11 18:41:16 +0100

    Revert script changes

--- run-helloworld.sh
+++ run-helloworld.sh
@@ -47,16 +47,16 @@ run_with() {
 if [ "x" != "x$J_ML" ]; then
 	echo
 	echo "Mainline: No barriers"
-#	run_with $J_ML $OPTS
+	run_with $J_ML $OPTS
 
 	echo
 	echo "Mainline: All barriers"
-#	run_with $J_ML $OPTS_ALL
+	run_with $J_ML $OPTS_ALL
 fi
 
 echo
 echo "LBE: No barriers"
-#run_with $J_LBE $OPTS
+run_with $J_LBE $OPTS
 
 echo
 echo "LBE: All barriers"
