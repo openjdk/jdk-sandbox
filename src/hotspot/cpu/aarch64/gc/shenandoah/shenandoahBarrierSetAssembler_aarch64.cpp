2e49ee9adfada218d13db3113ad28432149d75ec
Aleksey Shipilev <aleksey@shipilev.net>
2026-06-01 17:00:06 +0200

    AArch64: Support phase-specific barriers

--- src/hotspot/cpu/aarch64/gc/shenandoah/shenandoahBarrierSetAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/gc/shenandoah/shenandoahBarrierSetAssembler_aarch64.cpp
@@ -985,7 +985,7 @@ void ShenandoahBarrierStubC2::enter_if_gc_state(MacroAssembler& masm, const char
     // for real.
     __ nop();
   } else {
-    __ relocate(barrier_Relocation::spec());
+    __ relocate(barrier_Relocation::spec(), ShenandoahThreadLocalData::gc_state_to_fast_array_index(test_state));
     __ b(*entry());
 
 // #ifdef ASSERT
@@ -1128,11 +1128,10 @@ void ShenandoahBarrierStubC2::keepalive(MacroAssembler& masm, Label* L_done) {
   Address buffer(rthread, in_bytes(ShenandoahThreadLocalData::satb_mark_queue_buffer_offset()));
   Label L_through, L_slowpath;
 
-  // Hotpatched GC checks only care about idle/non-idle state, so we need to check again here.
-  __ ldrb(_tmp1, gcstate);
-  if (L_done != nullptr) {
-    maybe_far_jump_if_zero(masm, _tmp1, L_done);
-  } else {
+  // If another barrier is enabled as well, do a runtime check for a specific barrier.
+  if (_needs_load_ref_barrier) {
+    assert(L_done == nullptr, "L_done is always null when _needs_load_ref_barrier is true");
+    __ ldrb(_tmp1, gcstate);
     __ cbz(_tmp1, L_through);
   }
 
@@ -1183,11 +1182,13 @@ void ShenandoahBarrierStubC2::keepalive(MacroAssembler& masm, Label* L_done) {
 void ShenandoahBarrierStubC2::lrb(MacroAssembler& masm) {
   Label L_slow;
 
-  // Hotpatched GC checks only care about idle/non-idle state, so we need to check again here.
-  char state_to_check = ShenandoahHeap::HAS_FORWARDED | (_needs_load_ref_weak_barrier ? ShenandoahHeap::WEAK_ROOTS : 0);
-  Address gc_state_fast(rthread, in_bytes(ShenandoahThreadLocalData::gc_state_fast_array_offset(state_to_check)));
-  __ ldrb(_tmp1, gc_state_fast);
-  maybe_far_jump_if_zero(masm, _tmp1, continuation());
+  // If another barrier is enabled as well, do a runtime check for a specific barrier.
+  if (_needs_keep_alive_barrier) {
+    char state_to_check = ShenandoahHeap::HAS_FORWARDED | (_needs_load_ref_weak_barrier ? ShenandoahHeap::WEAK_ROOTS : 0);
+    Address gc_state_fast(rthread, in_bytes(ShenandoahThreadLocalData::gc_state_fast_array_offset(state_to_check)));
+    __ ldrb(_tmp1, gc_state_fast);
+    maybe_far_jump_if_zero(masm, _tmp1);
+  }
 
   // If weak references are being processed, weak/phantom loads need to go slow,
   // regardless of their cset status.
--- src/hotspot/cpu/aarch64/relocInfo_aarch64.hpp
+++ src/hotspot/cpu/aarch64/relocInfo_aarch64.hpp
@@ -34,7 +34,8 @@
     offset_unit        =  4,
     // Must be at least 1 for RelocInfo::narrow_oop_in_const.
     // Must be at least 2 for ZGC GC barrier patching.
-    format_width       =  2
+    // Must be at least 3 for Shenandoah GC barrier patching.
+    format_width       =  3
   };
 
  public:
