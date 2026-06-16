0c790b224309617a473a2bb3916dcf7777d1c4e1
Aleksey Shipilev <aleksey@shipilev.net>
2026-06-16 10:29:21 +0200

    RISC-V: Relocation format needs to fit Shenandoah barriers

--- src/hotspot/cpu/riscv/relocInfo_riscv.hpp
+++ src/hotspot/cpu/riscv/relocInfo_riscv.hpp
@@ -33,7 +33,8 @@
     offset_unit        =  1,
     // Must be at least 1 for RelocInfo::narrow_oop_in_const.
     // Must be at least 2 for ZGC GC barrier patching.
-    format_width       =  2
+    // Must be at least 3 for Shenandoah GC barrier patching.
+    format_width       =  3
   };
 
  public:
