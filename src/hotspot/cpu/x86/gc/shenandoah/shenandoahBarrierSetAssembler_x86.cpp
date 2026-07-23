9474d1fbc1397a618988cbdfe7e310de704cabdc
Aleksey Shipilev <aleksey@shipilev.net>
2026-07-23 12:18:30 +0200

    x86: Care about not binding the entries, like other architectures do it

--- src/hotspot/cpu/x86/gc/shenandoah/shenandoahBarrierSetAssembler_x86.cpp
+++ src/hotspot/cpu/x86/gc/shenandoah/shenandoahBarrierSetAssembler_x86.cpp
@@ -45,6 +45,7 @@
 #endif
 #ifdef COMPILER2
 #include "gc/shenandoah/c2/shenandoahBarrierSetC2.hpp"
+#include "opto/output.hpp"
 #endif
 
 #define __ masm->
@@ -769,6 +770,15 @@ void ShenandoahBarrierStubC2::cardtable(MacroAssembler& masm, Address addr, Regi
 }
 
 void ShenandoahBarrierStubC2::patchable_jump(MacroAssembler& masm, const char gc_state, bool jump_when_state, Label* L_target, bool needs_far_jump) {
+  PhaseOutput* const output = Compile::current()->output();
+  if (output->in_scratch_emit_size()) {
+    // We piggyback on scratch_emit_size mode to compute the slowpath stub size.
+    // Avoid binding entry() or trusting needs_far_jump at this time.
+    // We know the patched check is exactly 5 bytes long.
+    __ nop(5);
+    return;
+  }
+
   // Emit the unconditional branch in the first version of the method.
   // Let the rest of runtime figure out how to manage it.
   __ relocate(patchable_barrier_Relocation::spec(ShenandoahNMethod::encode_to_reloc(gc_state, jump_when_state)));
